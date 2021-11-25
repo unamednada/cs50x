bf2c443435a045f7e52686d3bdfdc7ada9267ce3
unamednada <dias.gustavo.dias@gmail.com>
2021-11-25 23:48:08 +0000

    Write header succesfully

--- pset4/lab4/volume.c
+++ pset4/lab4/volume.c
@@ -44,6 +44,7 @@ int main(int argc, char *argv[])
     {
         printf("%i ", header[i]);
     }
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
