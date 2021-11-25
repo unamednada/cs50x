f94261e0fcb1ff5e4167ae9ffea4b49ed9b495dc
unamednada <dias.gustavo.dias@gmail.com>
2021-11-25 23:41:31 +0000

    Start outline of volume.c

--- pset4/lab4/volume.c
+++ pset4/lab4/volume.c
@@ -35,6 +35,16 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
 
+    uint8_t header[HEADER_SIZE];
+    int16_t buffer;
+    uint8_t headerByte;
+
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    for (int i = 0; i < HEADER_SIZE; i++)
+    {
+        printf("%i ", header[i]);
+    }
+
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
